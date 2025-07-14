307ded5d80c2ccf4d09b0d7f22eeda2ec395c1fe
programmarimran <programmarimran@gmail.com>
2025-07-14 12:45:47 +0600

    added a google font implement is complited

--- src/shared/logo/Logo.jsx
+++ src/shared/logo/Logo.jsx
@@ -2,9 +2,9 @@ import logo from "../../assets/logo.png";
 
 const Logo = () => {
   return (
-    <div className=" flex gap-1 items-center">
+    <div className=" flex md:gap-1 items-center">
       <img className=" w-18" src={logo} alt="" />
-      <h2 className="text-3xl font-bold">
+      <h2 className="font-poppins -ml-2 text-lg md:text-3xl font-bold">
         W<span className="text-primary">o</span>rkSync
       </h2>
     </div>
